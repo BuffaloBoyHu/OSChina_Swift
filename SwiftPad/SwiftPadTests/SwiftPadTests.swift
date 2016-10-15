893522198db9ac74035bbe0c8a1b8f4ab892d8de
buffaloboyhu <buffaloboyhu@163.com>
2016-10-15 09:04:12 +0800

    创建工程

---
+++ SwiftPad/SwiftPadTests/SwiftPadTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftPadTests.swift
+//  SwiftPadTests
+//
+//  Created by hu lianghai on 2016/10/15.
+//  Copyright © 2016年 buffalo. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftPad
+
+class SwiftPadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
